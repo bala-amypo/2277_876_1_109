46575fec53da3665e099790ef2b04b70aca0f491
bala-amypo <bala@amypo.in>
2025-12-18 04:24:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -1,14 +1,121 @@
 package com.example.demo.entity;
 
-public class User {
-    private long id;
-    private String userId;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(
+    name = "user_profiles",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "userId"),
+        @UniqueConstraint(columnNames = "email")
+    }
+)
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;   // userId (unique)
+
+    @Column(nullable = false)
     private String fullName;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String role;
-    private Boolean active;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+  
+    @ManyToMany
+    @JoinTable(
+        name = "user_favourite_cards",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "card_id")
+    )
+    private Set<CreditCardRecord> favouriteCards = new HashSet<>();
 
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
+ 
+    public Boolean getActive() {
+        return active;
+    }
+ 
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+ 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Set<CreditCardRecord> getFavouriteCards() {
+        return favouriteCards;
+    }
+
+    public void setFavouriteCards(Set<CreditCardRecord> favouriteCards) {
+        this.favouriteCards = favouriteCards;
+    }
 }
