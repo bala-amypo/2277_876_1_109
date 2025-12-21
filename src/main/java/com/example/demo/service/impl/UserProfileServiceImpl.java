2b0787c99a52ce83e808bdc5cdd9fe53917010c0
bala-amypo <bala@amypo.in>
2025-12-21 07:43:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.UserProfileRepository;
 import com.example.demo.service.UserProfileService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
 
@@ -17,5 +19,20 @@ public class UserProfileServiceImpl implements UserProfileService {
     @Override
     public UserProfile save(UserProfile user) {
         return repository.save(user);
+    }
+
+    @Override
+    public Optional<UserProfile> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public void updateUserStatus(Long id, boolean active) {
+        Optional<UserProfile> userOpt = repository.findById(id);
+        if (userOpt.isPresent()) {
+            UserProfile user = userOpt.get();
+            user.setActive(active);
+            repository.save(user);
+        }
     }
 }
