69a951b29d4468f278899dabb8ecf0af6e1dc88a
bala-amypo <bala@amypo.in>
2025-12-24 03:45:57 +0000

    User commit

--- src/main/java/com/example/demo/service/UserProfileService.java
+++ src/main/java/com/example/demo/service/UserProfileService.java
@@ -1,13 +1,45 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.UserProfile;
-import java.util.List;
+import com.example.demo.repository.UserProfileRepository;
+import org.springframework.stereotype.Service;
 
-public interface UserProfileService {
+@Service
+public class UserProfileService {
 
-    UserProfile createUser(UserProfile user);       /
-    UserProfile getUserById(Long id);               
-    List<UserProfile> getAllUsers();                
-    void updateUserStatus(Long userId, boolean active); 
-    UserProfile findByUserId(String userId);       
+    private final UserProfileRepository userProfileRepository;
+
+    public UserProfileService(UserProfileRepository userProfileRepository) {
+        this.userProfileRepository = userProfileRepository;
+    }
+
+    public UserProfile createUser(UserProfile userProfile) {
+
+        if (userProfileRepository.existsByUserId(userProfile.getUserId())) {
+            throw new RuntimeException("UserId already exists");
+        }
+
+        if (userProfileRepository.existsByEmail(userProfile.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        return userProfileRepository.save(userProfile);
+    }
+
+    public UserProfile login(String email, String password) {
+
+        UserProfile user = userProfileRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!Boolean.TRUE.equals(user.getActive())) {
+            throw new RuntimeException("User is inactive");
+        }
+
+        if (!user.getPassword().equals(password)) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return user;
+    }
 }
